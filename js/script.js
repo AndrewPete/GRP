a85e143011994aa1ae053b0e903c87b46321a620
Bran Andrei Petru <branandreipetru@192-168-0-152.rdsnet.ro>
2017-10-17 15:54:54 +0300

    no message

--- js/script.js
+++ js/script.js
@@ -1,15 +1,20 @@
 $(function() {
   //Add the fixed class to header
-  set_header_white();
-  $(window).scroll(function() {
+  var grp_hero = $('#grp-hero');
+  if($(grp_hero).length) {
     set_header_white();
-  });
-  function set_header_white() {
-    var scroll = $(window).scrollTop();
-    if (scroll >= 70) {
-      $(".navbar").addClass("grp-navbar-white");
-    } else {
-      $(".navbar").removeClass("grp-navbar-white");
+    $(window).scroll(function() {
+      set_header_white();
+    });
+    function set_header_white() {
+      var grp_height = $(grp_hero).outerHeight();
+      var scroll_offset = grp_height - 100;
+      var scroll = $(window).scrollTop();
+      if (scroll >= scroll_offset) {
+        $(".navbar").addClass("grp-navbar-white");
+      } else {
+        $(".navbar").removeClass("grp-navbar-white");
+      }
     }
   }
 
